a4745dfb7172e470e09fc5ddacffc205566abe05
saurabhsharma <sauravh876sharma@gmail.com>
2024-08-09 15:53:09 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+module.exports = {
+    sqlConfig: {
+      user: 'bootcamp',
+      password: 'Pass@123',
+      server: 'bootcampaug5server.database.windows.net',
+      database: 'bootcampaug5db',
+      options: {
+        encrypt: true,
+        trustServerCertificate: false // Change to true if needed
+      }
+    }
+  };
+  
---
+++ index.js
@@ -0,0 +1,71 @@
+// index.js
+const express = require('express');
+const sql = require('mssql');
+const path = require('path');
+const config = require('./config');
+
+const app = express();
+const port = 8000;
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to handle home page
+app.get('/', async (req, res) => {
+    
+    res.render('index');
+});
+
+//Route to handle task2 page
+app.get('/task2', async (req, res) => {
+    try {
+      let pool = await sql.connect(config.sqlConfig);
+      let result = await pool.request().query('SELECT TOP 20 * FROM SalesLT.Customer');
+      
+      // Extract rows
+      let rows = result.recordset;
+      
+      // Extract columns
+      let columns = result.recordset.length > 0 
+        ? Object.keys(result.recordset[0])
+        : []; // Default to an empty array if no rows
+  
+      res.render('task2', { data: rows, columns: columns });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server Error');
+    }
+  });
+  
+
+// // Route to handle task3 page
+app.get('/task3', async (req, res) => {
+    try {
+      let pool = await sql.connect(config.sqlConfig);
+      let result = await pool.request().query(`
+        SELECT SalesLT.Product.Name AS ProductName, SalesLT.Product.Color, SalesLT.Product.Size, SalesLT.Product.Weight
+        FROM SalesLT.Product
+        JOIN SalesLT.ProductCategory ON SalesLT.Product.ProductCategoryID = SalesLT.ProductCategory.ProductCategoryID
+      `);
+  
+      // Extract rows
+      let rows = result.recordset;
+  
+      // Extract column names from the keys of the first row object
+      let columns = rows.length > 0 ? Object.keys(rows[0]) : [];
+  
+      res.render('task3', { data: rows, columns: columns });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server Error');
+    }
+  });
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
